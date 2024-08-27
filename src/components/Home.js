62ad3864dc0c974f58be5ca0b832a8cfc695125e
Goncalo Videira <goncalovideira@hotmail.com>
2024-08-27 18:01:10 +0100

    hashRouter with #

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
       <div className='container center section'>
         <h5 className="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor officiis id, ipsum doloribus distinctio sunt ducimus cumque nesciunt ullam aut. Veniam corrupti ab saepe expedita eos quos, incidunt fuga tempore?</h5>
-        <a class="waves-effect waves-light btn" href='/smiley-app/to-do'>Aceder</a>
+        <a class="waves-effect waves-light btn" href='/smiley-app/#/to-do'>Aceder</a>
       </div>
   );
 };
