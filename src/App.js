4726d83e25c094d9d1a20392e86fa91baac03e9c
Goncalo Videira <goncalovideira@hotmail.com>
2024-08-27 17:45:28 +0100

    trying hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { HashRouter as Router, Routes,Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import ToDoTable from './components/ToDoTable';
 import Home from './components/Home';
@@ -13,11 +13,8 @@ function App() {
       <Router>
         <div>
           <Routes>
-            <Route path="/smiley-app" element={<Home />} />
-            <Route path="/smiley-app/to-do" element={
-              <ToDoTable />
-
-            } />
+            <Route path="/" element={<Home />} />
+            <Route path="/to-do" element={<ToDoTable />} />
           </Routes>
         </div>
       </Router>
