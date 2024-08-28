99c552983f02be8847e963d1d536a4e1b53042b2
Goncalo Videira <goncalovideira@hotmail.com>
2024-08-28 16:25:21 +0100

    corrected date

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useCallback } from 'react';
 
 const Dashboard = () => {
   const [points, setPoints] = useState(0);
-  const [date, setDate] = useState('');
 
   const getPreviousDayPoints = useCallback(() => {
     const baseId = process.env.REACT_APP_BASE_ID;
@@ -15,11 +14,6 @@ const Dashboard = () => {
       return;
     }
 
-    const previousDay = new Date();
-    previousDay.setDate(previousDay.getDate() - 1);
-    setDate(previousDay.toISOString().split('T')[0]);
-
-    
     const filterFormula = `IS_SAME({Date}, DATEADD(TODAY(), -1, 'days'))`;
     const url = `https://api.airtable.com/v0/${baseId}/${encodeURIComponent(tableName)}?view=${encodeURIComponent(viewName)}&filterByFormula=${encodeURIComponent(filterFormula)}`;
 
@@ -35,8 +29,6 @@ const Dashboard = () => {
       .then(response => response.json())
       .then(data => {
         console.log('Fetched data:', data);
-        console.log('Previous day:', date);
-
         const totalPoints = data.records.reduce((sum, record) => 
           sum + (record.fields['Points net'] || 0), 0
         );
