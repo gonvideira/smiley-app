58e72f0f20659f1e7bde1fafe129dba4fcd6e2c7
Goncalo Videira <goncalovideira@hotmail.com>
2024-08-30 10:42:45 +0100

    removed icon ref

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,6 @@ function Footer() {
         <div className="row">
           © 2024 Gonçalo Videira
         </div>
-        <div className="row">
-          <a target="_blank" href="https://icons8.com/icon/8118/to-do" rel="noreferrer" className="white-text">To Do</a> <span className="white-text">icon by </span><a target="_blank" href="https://icons8.com" rel="noreferrer" className="white-text">Icons8</a>
-        </div>
       </div>
 
     </footer>
